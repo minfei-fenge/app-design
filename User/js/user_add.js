34b8dbc6b3837a91c3df2465ef874cb09e48a6f6
mj11331313 <2238178090@qq.com>
2017-09-11 14:02:13 +0800

    上传头像并保存功能已完成； 未完成：点击“保存”按钮，返回上一页并自动生成新建联系人名片

--- User/js/user_add.js
+++ User/js/user_add.js
@@ -1,21 +1,21 @@
 $(function(){
     /*上传照片*/
-    function fileUpLoad(){
-        var file = this.files[0];
-        if(!FileReader){
-            $("#myAlert span").html("你的浏览器不支持H5的FileReader");
-            $("#myAlert").show();
-            $("#upload").attr("disabled","disabled");
-            return;
-        }
-        var fileReader = new FileReader();
-        fileReader.readAsDataURL(file);//将文件读取为Data URL 读取结果放在result中
-        fileReader.onload = function(){
-            $("#upload-picture img").attr("src",this.result);
-            localStorage.setItem("user_add",this.result);
-        }
-    }
-    $("#upload").on("change",fileUpLoad);
+    document.getElementById("upload").addEventListener("change",function(e){
+        var files = this.files;
+        var img = new Image();
+        var reader = new FileReader();
+        reader.readAsDataURL(files[0]);
+        reader.onload = function(e){
+            var mb = (e.total/1024)/1024;
+            localStorage.setItem("user_add",e.target.result);
+            if(mb>= 2){
+                alert('文件大小大于2M');
+                return;
+            }
+            img.src = this.result;
+            img.style.width = "80%";
+        };
+    });
     /*保存头像*/
     if(localStorage.getItem("user_add")){
         $("#upload-picture img").attr("src",localStorage.getItem("user_add"));
@@ -30,5 +30,10 @@ $(function(){
             $(this).html("收起"+"<span class='caret'></span>");
         }
         $(this).prop("show",!$(this).prop("show"));
+    });
+
+    //点击“保存”按钮：
+    $("#save").on("click",function(){
+
     });
 });
