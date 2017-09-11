718deea8905a8951c3c964718a153af0f358f6e2
minfei-minfei <minfei-minfei@139.com>
2017-09-11 16:15:30 +0800

    用户端

--- User/js/user_index_circle.js
+++ User/js/user_index_circle.js
@@ -6,4 +6,6 @@ $(function(){
             $("#comment-box").hide();
         });
     });
+    /*添加事件：屏幕向下滚动时，nav背景变成白色，字体颜色变成黑色，#话题#添加到nav中间(样式已写好，修改html即可)*/
+    /*添加事件：点击星星切换class为glyphicon-star，颜色变成黄色，tooltip显示提示“已取消”*/
 });
