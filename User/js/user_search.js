0e9704a3c111e0f4e3b877a0fe69ae0a8ac22140
minfei-minfei <minfei-minfei@139.com>
2017-09-13 20:05:21 +0800

    护工搜索界面

--- User/js/user_search.js
+++ User/js/user_search.js
@@ -1,9 +1,14 @@
 $(function(){
+    //叉叉显示&隐藏
     $("#search-group input").on("keyup",function(){
-            $(this).siblings("span").css("display","inline");
+        $(this).siblings("span").css("display","inline");
     }).on("blur",function(){
         if($(this).val()==$(this)[0].defaultValue){
             $(this).siblings("span").css("display","none");
         }
+    });
+    //点击叉叉事件
+    $(".glyphicon-remove-circle").on("click",function(){
+        $(this).siblings("input").val("").trigger("blur");
     });
 });
