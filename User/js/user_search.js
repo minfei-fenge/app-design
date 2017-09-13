5e99d70479b2f106ea7af839f64613afb0777803
mj11331313 <2238178090@qq.com>
2017-09-13 19:23:55 +0800

    更新

--- User/js/user_search.js
+++ User/js/user_search.js
@@ -1,9 +1,23 @@
 $(function(){
-    $("#search-group input").on("keyup",function(){
+    $("#search-group input").on("keyup",function(e){
+        if( $(this).val()!= ""){
             $(this).siblings("span").css("display","inline");
+        }else{
+            $(this).siblings("span").hide();
+        };
+        if(e.which == 13 ){
+            if( $("#search-group input").val() != ""){
+                $("#search-group button").trigger("click");
+            }else{
+                alert("请输入内容！");
+            }
+        }
     }).on("blur",function(){
         if($(this).val()==$(this)[0].defaultValue){
             $(this).siblings("span").css("display","none");
-        }
+        };
+    });
+    $("#search-group span").on("click",function(){
+        $("#search-group input").val("").focus();
     });
 });
